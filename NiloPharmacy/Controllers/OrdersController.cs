5ac9ee7d5a081c57da6ccf8288f26ee335b9648a
Nilofar <nilofarsheriff@gmail.com>
2022-11-17 12:10:30 +0530

    Add project files.

---
+++ NiloPharmacy/Controllers/OrdersController.cs
@@ -0,0 +1,169 @@
+﻿
+using Rotativa;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using xyzpharmacy.Data.Cart;
+using xyzpharmacy.Data.Services;
+using xyzpharmacy.Data.Static;
+using xyzpharmacy.Data.ViewModels;
+using xyzpharmacy.Models;
+using System.Security.Claims;
+using Rotativa.NetCore;
+using Azure;
+
+namespace xyzpharmacy.Controllers
+{
+    [Authorize]
+    public class OrdersController : Controller
+    {
+        private readonly IProductsService _service;
+        private readonly ShoppingCart _shoppingCart;
+        private readonly IOrdersService _orderservice;
+
+
+        public OrdersController( IProductsService Service, ShoppingCart shoppingCart,IOrdersService ordersService)
+        {
+            _service = Service;
+            _shoppingCart = shoppingCart;
+            _orderservice = ordersService;
+        }
+
+        public async Task<IActionResult> ShoppingCart()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+            
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string userEmailAddress = User.FindFirstValue(ClaimTypes.Email);
+            List<Order> orderslist = await _orderservice.GetOrders();
+            List<Order> Found = orderslist.FindAll(x => x.UserId == userId);
+            if (Found.Count==0)
+            {
+                var response = new ShoppingCartVM()
+                {
+                    ShoppingCart = _shoppingCart,
+                    ShoppingCartTotal = (_shoppingCart.GetShoppingCartTotal()/2)
+
+                };
+               
+                return View(response);
+            }
+            else if (Found.Count>=3)
+            {
+                var response = new ShoppingCartVM()
+                {
+                    ShoppingCart = _shoppingCart,
+                    ShoppingCartTotal = (_shoppingCart.GetShoppingCartTotal()-20)
+                };
+                ViewBag.Price = response.ShoppingCartTotal;
+                return View(response);
+
+            }
+            else
+            {
+                var response = new ShoppingCartVM()
+                {
+                    ShoppingCart = _shoppingCart,
+                    ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+                };
+                ViewBag.Price = response.ShoppingCartTotal;
+                return View(response);
+            }
+           
+        }
+        public async Task<IActionResult> CompleteOrder()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string userEmailAddress = User.FindFirstValue(ClaimTypes.Email);
+            foreach(var item in items)
+            {
+               var list = await _service.GetAllAsync();
+               Product found = list.FirstOrDefault(X=>X.ProductId==item.product.ProductId)!;
+                found.Stock = (int)(found.Stock - item.Amount);
+                await _service.UpdateAsync(found.ProductId, found);
+
+            }
+            await _orderservice.StoreOrderAsync(items, userId, userEmailAddress);
+            await _shoppingCart.ClearShoppingCartAsync();
+
+            return View("OrderCompleted");
+        }
+        public async Task<IActionResult> Index()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string userRole = User.FindFirstValue(ClaimTypes.Role);
+
+            var orders = await _orderservice.GetOrdersByUserIdAndRoleAsync(userId, userRole);
+            return View(orders);
+        }
+    
+        public async Task<IActionResult> AddItemToShoppingCart(int id)
+        {
+            var item = await _service.GetByIdAsync(id);
+            
+
+            if (item != null)
+            {
+                _shoppingCart.AddItemToCart(item);
+               
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+
+        public async Task<IActionResult> RemoveItemFromShoppingCart(int id)
+        {
+            var item = await _service.GetByIdAsync(id);
+
+            if (item != null)
+            {
+                _shoppingCart.RemoveItemFromCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string userRole = User.FindFirstValue(ClaimTypes.Role);
+
+            var orders = await _orderservice.GetOrdersByUserIdAndRoleAsync(userId, userRole);
+            foreach(var order in orders)
+            {
+                foreach(var item in order.OrderItems)
+                {
+                    
+                        var list = await _service.GetAllAsync();
+                        Product found1 = list.FirstOrDefault(X => X.ProductId == item.product.ProductId)!;
+                        found1.Stock = (int)(found1.Stock + item.Amount);
+                        await _service.UpdateAsync(found1.ProductId, found1);
+
+                    
+                }
+            }
+            //return View(orders);
+            //List<Order> orders = await _orderservice.GetOrders();
+            Order found = orders.FirstOrDefault(X => X.Id == Id)!;
+            return View(found);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            List<Order> orderlist = await _orderservice.GetOrders();
+            if (orderlist == null)
+            {
+                return View("NotFound");
+            }
+            await _orderservice.DeleteAsync(id);
+            
+
+            return View("OrderCancellation"); ;
+
+
+
+
+        }
+
+    }
+}
---
+++ NiloPharmacy/Controllers/ProductsController.cs
@@ -0,0 +1,197 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using xyzpharmacy.Data;
+using Microsoft.AspNetCore.Authorization;
+using xyzpharmacy.Data.Services;
+using xyzpharmacy.Models;
+using xyzpharmacy.Data.Static;
+using System.Security.Cryptography;
+using XYZPharmacy.Data.ViewModels;
+
+namespace xyzpharmacy.Controllers
+{
+    [Authorize(Roles = UserRoles.Admin)]
+    public class ProductsController : Controller
+    {
+        
+
+        
+        private readonly IProductsService _service;
+
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public ProductsController(IProductsService Service, IWebHostEnvironment hostEnvironment)
+        {
+            _service = Service;
+            webHostEnvironment = hostEnvironment;
+        }
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            ViewData["Welcome"] = "Welcome to our XYZ Pharmacy";
+            ViewBag.Description = "XYZ Pharmacy is your go-to online pharmacy store " +
+                "for all your medicine needs – be it your regular medications, or over-the-counter (OTC) medicines.";
+            var data = await _service.GetAllAsync();
+            return View(data);
+        }
+
+        public async Task<IActionResult> CreateAsync()
+        {
+            
+            var movieDropdownsData = await _service.GetNewProductsDropdownsValues();
+
+            ViewBag.Suppliers = new SelectList(movieDropdownsData.Suppliers, "SupplierId", "SupplierName");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductsViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = "~/Image/" + UploadedFile(model);
+
+                Product prod = new Product()
+                {
+                    ProductName = model.ProductName,
+                    ProductPrice = model.ProductPrice,
+                    CategoryName = model.CategoryName,
+                    SupplierId = model.SupplierId,
+                    Stock = model.Stock,
+                    ExpiryDate = model.ExpiryDate,
+                    MedicinalUse = model.MedicinalUse,
+                    MedicineDesc = model.MedicineDesc,
+                    ProductImage = uniqueFileName,
+                };
+
+                await _service.AddAsync(prod);
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Details(int Id)
+        {
+            var SupplierDetails = await _service.GetByIdAsync(Id);
+            if (SupplierDetails == null)
+            {
+                return View("Not Found");
+            }
+            return View(SupplierDetails);
+        }
+
+
+        public async Task<IActionResult> Edit(int Id)
+        {
+            var movieDropdownsData = await _service.GetNewProductsDropdownsValues();
+
+            ViewBag.Suppliers = new SelectList(movieDropdownsData.Suppliers, "SupplierId", "SupplierName");
+            var model = await _service.GetByIdAsync(Id);
+            if (model == null)
+            {
+                return View("Not Found");
+            }
+            ProductsViewModel supplier = new ProductsViewModel()
+            {
+                ProductName = model.ProductName,
+                ProductPrice = model.ProductPrice,
+                CategoryName = model.CategoryName,
+                SupplierId = model.SupplierId,
+                Stock = model.Stock,
+                ExpiryDate = model.ExpiryDate,
+                MedicinalUse = model.MedicinalUse,
+                MedicineDesc = model.MedicineDesc,
+            };
+
+            return View(supplier);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ProductsViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = "~/Image/" + UploadedFile(model);
+
+                Product prod = new Product()
+                {
+                    ProductName = model.ProductName,
+                    ProductPrice = model.ProductPrice,
+                    CategoryName = model.CategoryName,
+                    SupplierId = model.SupplierId,
+                    Stock = model.Stock,
+                    ExpiryDate = model.ExpiryDate,
+                    MedicinalUse = model.MedicinalUse,
+                    MedicineDesc = model.MedicineDesc,
+                    ProductImage = uniqueFileName,
+                };
+
+                await _service.UpdateAsync(id, prod);
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+        [AllowAnonymous]
+        public async Task<IActionResult> Filter(string searchString)
+        {
+            var allProd = await _service.GetAllAsync();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                
+                var filteredResult = allProd.Where(n => n.ProductName.Contains(searchString) || n.MedicineDesc.Contains(searchString) || n.MedicinalUse.ToString().Contains(searchString) || n.CategoryName.ToString().Contains(searchString)).ToList();
+                //var filteredResultNew = allProd.Where(n => string.Equals(n.ProductName, searchString, StringComparison.CurrentCultureIgnoreCase) || string.Equals(n.MedicineDesc, searchString,
+                //    StringComparison.CurrentCultureIgnoreCase)||(string.Equals(n.CategoryName.ToString(), searchString, StringComparison.CurrentCultureIgnoreCase))).ToList();
+                return View("Index", filteredResult);
+                
+            }
+
+            return View("Index", allProd);
+        }
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var supplier = await _service.GetByIdAsync(Id);
+            if (supplier == null)
+            {
+                return View("NotFound");
+            }
+            return View(supplier);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int Id)
+        {
+            var supplier = await _service.GetByIdAsync(Id);
+            if (supplier == null)
+            {
+                return View("NotFound");
+            }
+            await _service.DeleteAsync(Id);
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        private string UploadedFile(ProductsViewModel model)
+        {
+            string uniqueFileName = null;
+
+            if (model.ProductImage != null)
+            {
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "Image");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ProductImage.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.ProductImage.CopyTo(fileStream);
+                }
+            }
+            return uniqueFileName;
+        }
+    }
+}
